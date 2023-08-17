e84d2aaeac17a81ced5ad2f9a8c090116453beda
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2023-08-17 19:32:58 -0300

    added rool event

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    var images = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageViewOne.alpha = 0.8
+        diceImageViewTwo.alpha = 0.8
+                
+        diceImageViewOne.image = images.first
+        diceImageViewTwo.image = images.first
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageViewOne.image = images[Int.random(in: 0...5)]
+        diceImageViewTwo.image = images[Int.random(in: 0...5)]
+    }
 }
 
